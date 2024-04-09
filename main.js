b152f3200d8ce7eec4640f76cd5132b59ba0e5a5
jordan williamson <williamsonjordan05@gmail.com>
2024-04-09 16:27:59 -0600

    Iteration 3 completed
    Co-authored-by: Peter Kim, <peterkim.pk1@gmail.com>
    Co-authored-by: Kim Ewing, <pekar.kimberly@gmail.com>
    Co-authored-by: Marshall Hotaling, <marshall.hotaling@gmail.com>
    Co-authored-by: Jordan Williamson, <williamsonjordan05@gmail.com>

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var userInputs = document.querySelectorAll('input')
 var rectangle = document.querySelector('.rectangle');
 var savedCard = document.querySelector('.saved-card');
 var deleteButton = document.querySelector('.delete');
+var favoriteButton = document.querySelectorAll('.notFavorited')
 
 
 
@@ -24,11 +25,18 @@ saveButton.addEventListener('click', function() {
     toggleButton();
 })
 
+favoriteButton.forEach(function(notFavorited){
+    notFavorited.addEventListener('click', function (event){
+    event.preventDefault()
+    favoriteIcon(notFavorited);
+    });
+});
 
-// saveButton.setAttribute('disabled', 'disabled');
+
+
 
 savedCardSection.addEventListener('click', deleteCards);
-
+// savedCardSection.addEventListener('click', favoriteCards)
 
 userInputs[0].addEventListener('change', toggleButton)
 userInputs[1].addEventListener('change', toggleButton)
@@ -36,19 +44,46 @@ userInputs[1].addEventListener('change', toggleButton)
 // functions here
 // We need a function that can implement the user inputs into the html section and save them to the data model
 
-function displayUserCards(){
-    for (var i = 0; i < savedCards.length; i++) {
-        savedCardSection.innerHTML += `
-        <div class="saved-card">
+function displayUserCards() {
+    savedCardSection.innerHTML = ''; // Clear the section before adding new cards
+    savedCards.forEach(function(card, index) {
+        // Determine the favorite icon based on the isFavorite property
+        var favoriteIconSrc = card.isFavorite ? 'assets/star-active.svg' : 'assets/star.svg';
+
+        // Create the card HTML
+        var cardHTML = `
+        <div class="saved-card" data-index="${index}">
             <div class="rectangle">
-                <img class="favorite" src="assets/star.svg" alt="star symbol">
+                <img class="notFavorited" src="${favoriteIconSrc}" alt="star symbol">
                 <img class="delete" src="assets/delete.svg" alt="delete symbol">
             </div>
-          <h4 class="saved-card-title">${savedCards[i].title}</h4>
-          <p class="saved-card-info">${savedCards[i].body}</p>         
-        </div>`
-    }
+          <h4 class="saved-card-title">${card.title}</h4>
+          <p class="saved-card-info">${card.body}</p>         
+        </div>`;
+
+        // Add the card HTML to the savedCardSection
+        savedCardSection.innerHTML += cardHTML;
+    });
+
+    // Attach the event listeners to the favorite buttons of the newly added cards
+    attachFavoriteButtonListeners();
 }
+
+function attachFavoriteButtonListeners() {
+    var favoriteButtons = document.querySelectorAll('.notFavorited');
+    favoriteButtons.forEach(function(button, index) {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            favoriteIcon(button);
+            // Update the card's favorite status in the savedCards array
+            var cardIndex = button.closest('.saved-card').dataset.index;
+            savedCards[cardIndex].isFavorite = !savedCards[cardIndex].isFavorite; // Toggle the favorite status
+        });
+    });
+}
+
+
+
 
 function saveCards(){
     if (userTitle.value.length < 15) {
@@ -56,6 +91,7 @@ function saveCards(){
             id: Date.now(),
             title: userTitle.value,
             body: userBody.value,
+            isFavorite: false,
         }
         savedCards.push(cards);
         displayUserCards();
@@ -79,9 +115,18 @@ function toggleButton(){
     }
 }
 
+function displayFavoritedCards() {
+    var favoritedCards = savedCards.filter(function(card) {
+        return card.isFavorite;
+    });
+    savedCards = favoritedCards; 
+    displayUserCards(); 
+}
+
+
 function deleteCards(event){
     event.preventDefault();
-    var index = event.target.parentElement.parentElement.id
+    var index = event.target.parentElement.parentElement.id;
     console.log('line76:',index)
     if (event.target.className === 'delete') {
         event.target.parentElement.parentElement.remove();
@@ -89,3 +134,18 @@ function deleteCards(event){
     }
 }
 
+
+function favoriteIcon(notFavorited) {
+    var currentIcon = notFavorited.getAttribute('src');
+    var newIcon;
+
+    if(currentIcon === 'assets/star.svg'){
+        newIcon = 'assets/star-active.svg';
+        savedCards.isFavorite = true;
+    } else {
+        newIcon = 'assets/star.svg'
+        savedCards.isFavorite = false;
+    }
+
+    notFavorited.setAttribute('src', newIcon)
+}
